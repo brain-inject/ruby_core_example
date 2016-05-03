f8f3c4eaf3e93577dbaadd82db1562a7b5a5c245
JackMarx <seriousfools@gmail.com>
2016-05-02 21:16:59 -0500

    added arrays

---
+++ employee.rb
@@ -0,0 +1,6 @@
+
+employee1 = ["John", "Smith", 70000, true]
+employee2 = ["Jane", "doe", 80000, true]
+
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
